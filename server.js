a26ee59edfdaf6c656e33839bbfa4fe6a4f43acf
Alexander Kyle Chua <kyle.chua16@gmail.com>
2017-06-27 23:06:46 -0700

    server side requests

--- server.js
+++ server.js
@@ -1,36 +1,53 @@
 const http = require('http');
+const path = require('path');
+const url = require('url');
 const fs = require('fs');
 var scraper = require('./scraper.js');
 
 var server = http.createServer().listen(8080);
 
-// Later, do this all thru server request
-var summonerName = 'KyIe Chua';
-var queueTypes = [420,440];
-
 // Database which stores exchange rates and modifier
-var databaseURL = 'data/summoners/' + summonerName + '.json';
-
-var db = {};
-scraper.getStats(summonerName, queueTypes, db).then((result) =>{
-    db = result;
-    fs.writeFileSync(databaseURL, JSON.stringify(db));
-});
-
-/*
-// Parse Database
 var db;
-try {
-    db = JSON.parse(fs.readFileSync(databaseURL, 'utf8'));
-} catch(e) {
-    // Create JSON if file URL is invalid
-    db = {};
-    db = scraper.getRankedStats(summonerName, db);
-    fs.writeFileSync(databaseURL, JSON.stringify(db));
-}
-*/
 
-/*
-// Initialize Server
-var server = http.createServer().listen(8888);
-*/
+server.on('request', function(req, res){
+    var myURL = url.parse(req.url, true);
+    var myPath = path.parse(req.url);
+    var pathStr = path.format(myPath);
+
+    if (pathStr.indexOf('/rankedstats') != -1){
+        if (pathStr.indexOf('/retrieve/') != -1){
+
+            var queueTypes = myURL.query.queue;
+            var queues;
+            if (queueTypes == "flex"){
+                queues = [440];
+            } else if (queueTypes == "solo"){
+                queues = [420];
+            } else {
+                queues = [420,440];
+            }
+
+            var summonerName = path.parse(myURL.pathname).base;
+            var databaseURL = 'data/summoners/' + summonerName + '.json';
+            console.log(databaseURL);
+            try {
+                db = JSON.parse(fs.readFileSync(databaseURL, 'utf8'));
+                console.log("Found database.")
+                res.json(db); // <-- this is part of express.json
+            } catch(e) {
+                console.log(e)
+                scraper.getStats(summonerName, queues, db).then((result) =>{
+                    db = result;
+                    fs.writeFileSync(databaseURL, JSON.stringify(db));
+                    res.json(db);
+                }).catch(function(e){
+                    console.log("Unable to retrieve stats.");
+                });
+            }
+        }
+    } else {
+        // Not found, wrong URI
+        // response.statusCode = 404;
+    }
+    res.end();
+});
